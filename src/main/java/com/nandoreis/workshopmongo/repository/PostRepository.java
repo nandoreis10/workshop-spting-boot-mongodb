8422485d0a337f9c5934ecef344b7cf58dd2af68
nandoreis10 <fernandopessoal@hotmail.com>
2023-01-16 18:39:31 -0300

    Consulta simples com query methods

--- src/main/java/com/nandoreis/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nandoreis/workshopmongo/repository/PostRepository.java
@@ -1,12 +1,14 @@
 package com.nandoreis.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.nandoreis.workshopmongo.domain.Post;
-import com.nandoreis.workshopmongo.domain.User;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	public List<Post> findByTitleContainingIgnoreCase(String text);
 }
