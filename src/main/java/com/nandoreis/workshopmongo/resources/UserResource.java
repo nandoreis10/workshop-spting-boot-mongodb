c8c2d5a15b7ab4192367a458e5962082e9c20a31
nandoreis10 <fernandopessoal@hotmail.com>
2023-01-16 14:02:14 -0300

    Alteração para Spring Boot 3.0

--- src/main/java/com/nandoreis/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nandoreis/workshopmongo/resources/UserResource.java
@@ -1,31 +1,28 @@
 package com.nandoreis.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nandoreis.workshopmongo.domain.User;
+import com.nandoreis.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	// @RequestMapping(method=RequestMethod.GET)
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){		
-		User maria = new User("1", "Maria Brown", "maria@gmail.com");
-		User alex = new User("2", "Alex Green", "alex@gmail.com");
-		
-		List list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex));
-		
-		return ResponseEntity.ok().body(list);
-		
+		List<User> list = service.findAll();		
+		return ResponseEntity.ok().body(list);		
 	}
 
 }
